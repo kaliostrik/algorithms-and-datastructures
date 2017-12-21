528a0d5471c4709a6f2598ba14866aff5e529e1e
Your Name <you@example.com>
2017-12-21 14:53:32 +0300

    dfdsfsadf

---
+++ src/main/java/com/vsuet/utils/algo/search/LinearSearch.java
@@ -0,0 +1,18 @@
+package com.vsuet.utils.algo.search;
+
+public class LinearSearch {
+    public void  main(String[] args) {
+        int[] array = {1,2,3,4,5,9,77,55,33,10};
+        search(array, 77);
+    }
+
+    public int search(int[] array, int find) {
+        for (int i = 0; i != array.length; i = i + 1) {
+            if (find == array[i]) {
+                return i;
+            }
+        }
+
+        return -777;
+    }
+}
